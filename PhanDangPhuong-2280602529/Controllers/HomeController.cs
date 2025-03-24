164ed6620dfa79720590f0204831b38d686844fc
Phanphuong2004 <phanphuong19052004@gmail.com>
2025-03-25 04:43:12 +0700

    v

---
+++ PhanDangPhuong-2280602529/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using PhanDangPhuong_2280602529.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhanDangPhuong_2280602529.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
