0264902742ffb833d9ee8c4898a45a6ae5409eff
Phanphuong2004 <phanphuong19052004@gmail.com>
2025-03-25 06:09:45 +0700

    .

---
+++ 2280602529_PhanDangPhuong_tuan04/PhanDangPhuong-2280602529/Migrations/20250318014316_Initial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhanDangPhuong_2280602529.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ PhanDangPhuong-2280602529/PhanDangPhuong-2280602529/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PhanDangPhuong_2280602529.Models;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext>
+   options) : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<ProductImage> ProductImages { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Seed dữ liệu mẫu cho Category
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Điện tử" },
+            new Category { Id = 2, Name = "Gia dụng" },
+            new Category { Id = 3, Name = "Thời trang" }
+        );
+    }
+}
